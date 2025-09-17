5e10dbbd4ebc1d4f03784aad0719f6c32844f9bc
Andreas R. Olsen <andreasredsoeolsen@gmail.com>
2025-09-17 19:50:32 +0200

    Test for random ASCII files added

--- src/test.sh
+++ src/test.sh
@@ -10,13 +10,19 @@ echo "Generating a test_files directory.."
 mkdir -p test_files
 rm -f test_files/*
 
-
+# Defaultly included test files:
 echo "Generating test files.."
 printf "Hello, World!\n" > test_files/ascii.input
 printf "Hello, World!" > test_files/ascii2.input
 printf "Hello,\x00World!\n" > test_files/data.input
 printf "" > test_files/empty.input
-### TODO: Generate more test files ###
+
+# ASCII test files:
+for i in $(seq 1 20); do # Generates 20 files
+    bytes=$((i * 200)) # file sizes of 200, 400, 600... bytes
+    LC_ALL=C tr -dc 'A-Za-z0-9[:punct:][:space:]' < /dev/urandom | head -c "$bytes" > "test_files/ascii_rand_${i}.input" 
+    # Random content of only ascii characters
+done
 
 
 echo "Running the tests.."
