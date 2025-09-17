e93eb1b05741c9b44fd68bbe9f935354ee3f3c63
zako0001 <zako0001@stud.kea.dk>
2025-09-17 13:25:24 +0200

    oops

--- src/file.c
+++ src/file.c
@@ -4,22 +4,98 @@
 #include <errno.h>  // errno.
 #include <assert.h>
 
-int print_error(char *path, int errnum)
-{
+int print_error(char *path, int errnum) {
     return fprintf(stdout, "%s Somethings wrong (%s)\n", path, strerror(errnum));
 }
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "Usage: file path\n");
+        return EXIT_FAILURE;
+    }
     FILE *f = fopen(argv[1], "r");
     unsigned char c;
-    while (fread(&c, sizeof(char), 1, f) == 1)
-    {
-        printf("%3d ", (int)c);
-        if (c > 31 && c < 127)
-        {
-            fwrite(&c, sizeof(char), 1, stdout);
-        }
-        printf("\n");
+
+    // Testing for empty
+    if (fread(&c, sizeof(char), 1, f) == 0) {
+        printf("%s: empty\n", argv[1]);
+        return EXIT_SUCCESS;
     }
+
+    // Testing for ascii
+    fseek(f, 0, SEEK_SET); // returns non-zero if error
+    int isAscii = 1;
+    while (fread(&c, sizeof(char), 1, f) == 1) {
+        if (c < 7 || (c > 13 && c < 27) || (c > 27 && c < 32) || c > 127) {
+            isAscii = 0;
+            break;
+        }
+    }
+    if (isAscii == 1) {
+        printf("%s: ASCII text\n", argv[1]);
+        return EXIT_SUCCESS;
+    }
+
+    // Testing for ISO
+    fseek(f, 0, SEEK_SET);
+    int isIso = 1;
+    while (fread(&c, sizeof(char), 1, f) == 1) {
+        if (c < 7 || (c > 13 && c < 27) || (c > 27 && c < 32) || (c > 127 && c < 160)) {
+            isIso = 0;
+            break;
+        }
+    }
+    if (isIso == 1) {
+        printf("%s: ISO-8859 text\n", argv[1]);
+        return EXIT_SUCCESS;
+    }
+
+    // Testing for UTF
+    fseek(f, 0, SEEK_SET);
+    int isUtf = 1;
+    while (fread(&c, sizeof(char), 1, f) == 1) {
+        if (c > 247 || (c > 127 && c < 192)) {
+            isUtf = 0;
+            break;
+        } else if (c > 191 && c < 224) {
+            fread(&c, sizeof(char), 1, f);
+            if (c < 128 || c > 191) {
+                isUtf = 0;
+                break;
+            }
+        } else if (c > 223 && c < 240) {
+            fread(&c, sizeof(char), 1, f);
+            if (c < 128 || c > 191) {
+                isUtf = 0;
+                break;
+            }
+            fread(&c, sizeof(char), 1, f);
+            if (c < 128 || c > 191) {
+                isUtf = 0;
+                break;
+            }
+        } else if (c > 239 && c < 248) {
+            fread(&c, sizeof(char), 1, f);
+            if (c < 128 || c > 191) {
+                isUtf = 0;
+                break;
+            }
+            fread(&c, sizeof(char), 1, f);
+            if (c < 128 || c > 191) {
+                isUtf = 0;
+                break;
+            }
+            fread(&c, sizeof(char), 1, f);
+            if (c < 128 || c > 191) {
+                isUtf = 0;
+                break;
+            }
+        }
+    }
+    if (isUtf == 1) {
+        printf("%s: UTF-8 Unicode text\n", argv[1]);
+        return EXIT_SUCCESS;
+    }
+    printf("%s: data\n", argv[1]);
+    return EXIT_SUCCESS;
 }
